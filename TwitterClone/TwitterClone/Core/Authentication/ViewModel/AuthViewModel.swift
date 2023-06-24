6da3505a8dea62491ab584fcab1bf54da8cfa4f7
Oleksii <oleksii.irzhavskyi@gmail.com>
2023-06-24 19:47:04 +0300

    photopicker

--- TwitterClone/TwitterClone/Core/Authentication/ViewModel/AuthViewModel.swift
+++ TwitterClone/TwitterClone/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -10,6 +10,8 @@ import Firebase
 
 class AuthViewModel: ObservableObject {
     @Published var userSession: FirebaseAuth.User?
+    @Published var didAuthenticateUser = false
+    private var tempUserSession: FirebaseAuth.User?
     
     init() {
         self.userSession = Auth.auth().currentUser
@@ -39,10 +41,8 @@ class AuthViewModel: ObservableObject {
             }
             
             guard let user = result?.user else {return}
-            self.userSession = user
-            
-            print("DEBUG: Registered user successfully")
-            print("DEBUG: User is \(self.userSession)")
+            //self.userSession = user
+            self.tempUserSession = user
             
             let data = ["email": email,
                         "username": username.lowercased(),
@@ -52,7 +52,7 @@ class AuthViewModel: ObservableObject {
             Firestore.firestore().collection("users")
                 .document(user.uid)
                 .setData(data) { _ in
-                    print("DEBUG: Did upload user data")
+                    self.didAuthenticateUser = true
                 }
             
         }
@@ -61,5 +61,17 @@ class AuthViewModel: ObservableObject {
     func signOut() {
         self.userSession = nil
         try? Auth.auth().signOut()
+    }
+    
+    func uploadProfileImage(_ image: UIImage) {
+        guard let uid = tempUserSession?.uid else { return }
+        
+        ImageUploader.uploadImage(image: image) { profileImageUrl in
+            Firestore.firestore().collection("users")
+                .document(uid)
+                .updateData(["profileImageUrl": profileImageUrl]) { _ in
+                    self.userSession = self.tempUserSession
+                }
+        }
     }
 }
---
+++ TwitterClone/TwitterClone/Core/Authentication/Views/ProfilePhotoSelectroView.swift
@@ -0,0 +1,93 @@
+//
+//  ProfilePhotoSelectroView.swift
+//  TwitterClone
+//
+//  Created by Oleksii Irzhavskyi on 23.06.2023.
+//
+
+import SwiftUI
+
+struct ProfilePhotoSelectroView: View {
+    @State private var showImagePicker = false
+    @State private var selectedImage: UIImage?
+    @State private var profileImage: Image?
+    @EnvironmentObject var viewModel: AuthViewModel
+    var body: some View {
+        VStack {
+            AuthHeaderView(title1: "Setup account",
+                           title2: "Select a profile photo")
+            
+            Button {
+                print("Pick image here...")
+            } label: {
+                if let profileImage = profileImage {
+                    profileImage
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(Color(.systemBlue))
+                        .scaledToFill()
+                        .frame(width: 180, height: 180)
+                        .padding(.top, 40)
+                        .clipShape(Circle())
+                } else {
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(Color(.systemBlue))
+                        .scaledToFill()
+                        .frame(width: 180, height: 180)
+                        .padding(.top, 40)
+                        .clipShape(Circle())
+                }
+                
+            }
+            .sheet(isPresented: $showImagePicker,
+                   onDismiss: loadImage) {
+                ImagePicker(selectedImage: $selectedImage)
+            }
+            
+            if let selectedImage = selectedImage {
+                Button {
+                    viewModel.uploadProfileImage(selectedImage)
+                } label: {
+                    Text("Continue")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 340, height: 50)
+                        .background(Color(.systemBlue))
+                        .clipShape(Capsule())
+                        .padding()
+                }
+                .shadow(color: .gray.opacity(0.5), radius: 10, x: 0, y: 0)
+            }
+
+            
+            Spacer()
+        }
+        .ignoresSafeArea()
+    }
+    
+    func loadImage() {
+        guard let selectedImage = selectedImage else { return }
+        profileImage = Image(uiImage: selectedImage)
+    }
+}
+//
+//private struct ProfileImageModifier: ViewModifier {
+//    func body(content: Content) -> some View {
+//        content
+//            .resizable()
+//            .renderingMode(.template)
+//            .foregroundColor(Color(.systemBlue))
+//            .scaledToFill()
+//            .frame(width: 180, height: 180)
+//            .padding(.top, 40)
+//            .clipShape(Circle())
+//    }
+//}
+
+struct ProfilePhotoSelectroView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfilePhotoSelectroView()
+    }
+}
